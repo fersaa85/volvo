53e0b541210aa899fcd9d13c73a76d1577db32e5
MacBook_Air <macbook_air@MacBook-Air-de-MacBook-Air.local>
2022-09-25 05:35:23 -0500

    store

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,9 @@ Route::post('signup',  [AuthController::class, 'signUp']);
 Route::get('/', function () {
     return view('poll.home');
 });
-Route::get('encuesta', function () {
+Route::get('/encuesta', function () {
     return view('poll.store');
-});
+})->name('encuesta');
 Route::post('store',  [PollController::class, 'store']);
 
 
