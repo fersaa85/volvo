f0521aaca0525588d61bfd3caf35b284c2ab5b8c
fersaa85 <fersaavedra85@hotmail.com>
2023-06-19 11:14:34 -0600

    performances lazy loading

--- resources/views/poll/confirmed.blade.php
+++ resources/views/poll/confirmed.blade.php
@@ -6,7 +6,7 @@
 
         <div class="d-flex align-items-center justify-content-center" style="height: 350px">
             <div class="bd-highlight col-example">
-                <div class="success text-center">p
+                <div class="success text-center">
                     <h1 class="color-red text-uppercase text-center"><b>Gracias por participar en el Reto del Arroz Americano Auténtico</b></h1>
 
                     <h4>Tus respuestas fueron guardadas.</h4>
@@ -19,57 +19,5 @@
 
 
 </div>
-
 @stop
 
-@section('footer')
-    <div class="container">
-        <div class="row">
-            <div class="col-6">
-                <div class="row">
-                    <div class="col-4">
-                        <img data-src="{{URL::asset('/images/poll/Logo-SOS-200x200px.png')}}"  loading="lazy" class="img-fluid lazyload">
-                    </div>
-                    <div class="col-4">
-                        <img data-src="{{URL::asset('/images/poll/Logo-La-Merced-200x200px.png')}}"  loading="lazy" class="img-fluid lazyload">
-                    </div>
-
-                    <div class="col-4">
-                        <img data-src="{{URL::asset('/images/poll/Logo-Vereda-200x200px.png')}}" loading="lazy" class="img-fluid lazyload">
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 text-right" >
-                <img data-src="{{URL::asset('/images/poll/Logo-Arroz-Americano-Completo-600x200px.png')}}"  loading="lazy" class="img-fluid lazyload">
-            </div>
-        </div>
-    </div>
-@show
-
-@section('script')
-<script>
-
-    window.setTimeout(function(){
-
-        // Move to a new location or you can do something else
-        window.location.href = "https://wedrivetogether.mx/arroz";
-
-    }, 3000);
-
-    if ("loading" in HTMLImageElement.prototype) {
-        // Si el navegador soporta lazy-load, tomamos todas las imágenes que tienen la clase
-        // `lazyload`, obtenemos el valor de su atributo `data-src` y lo inyectamos en el `src`.
-        const images = document.querySelectorAll("img.lazyload");
-        images.forEach((img) => {
-            img.src = img.dataset.src;
-        });
-    } else {
-        // Importamos dinámicamente la libreria `lazysizes`
-        let script = document.createElement("script");
-        script.async = true;
-        script.src =
-            "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.0/lazysizes.min.js";
-        document.body.appendChild(script);
-    }
- </script>
-@stop
--- resources/views/poll/template.blade.php
+++ resources/views/poll/template.blade.php
@@ -114,26 +114,26 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-@section('script')
-    <script>
-        if ("loading" in HTMLImageElement.prototype) {
-            // Si el navegador soporta lazy-load, tomamos todas las imágenes que tienen la clase
-            // `lazyload`, obtenemos el valor de su atributo `data-src` y lo inyectamos en el `src`.
-            const images = document.querySelectorAll("img.lazyload");
-            images.forEach((img) => {
-                img.src = img.dataset.src;
-            });
-        } else {
-            // Importamos dinámicamente la libreria `lazysizes`
-            let script = document.createElement("script");
-            script.async = true;
-            script.src =
-                "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.0/lazysizes.min.js";
-            document.body.appendChild(script);
-        }
-    </script>
+<script>
+    if ("loading" in HTMLImageElement.prototype) {
+        // Si el navegador soporta lazy-load, tomamos todas las imágenes que tienen la clase
+        // `lazyload`, obtenemos el valor de su atributo `data-src` y lo inyectamos en el `src`.
+        const images = document.querySelectorAll("img.lazyload");
+        images.forEach((img) => {
+            img.src = img.dataset.src;
+        });
+    } else {
+        // Importamos dinámicamente la libreria `lazysizes`
+        let script = document.createElement("script");
+        script.async = true;
+        script.src =
+            "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.0/lazysizes.min.js";
+        document.body.appendChild(script);
+    }
+</script>
 
-@stop
+@section('script')
+@show
 
 </body>
 </html>
